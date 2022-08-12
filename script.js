858ae7fbf1bfbabcc27e128a436198d1edbbd8cb
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-08-12 17:38:53 -0400

    confirms data request

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 // Constant
+const URL = 'https://digimon-api.vercel.app/api/digimon/name/';
 
 // State
 
 // Cached element references
+const $main = $('main');
+const $form = $('form');
+const $input = $('input[type="text"]');
 
 // Event listeners
+$form.on('submit', handleSubmit);
+
+
 
 // Functions
+
+function handleSubmit(event) {
+    event && event.preventDefault();
+    
+    const name = $input.val()
+
+    if(!name) return;
+
+    const promise = $.ajax(`${URL}${name}`);
+
+    promise.then(
+        function(data){
+            console.log('Data: ', data);
+        },
+        function(error){
+            console.log('Error: ', error);
+        }
+    )
+}
