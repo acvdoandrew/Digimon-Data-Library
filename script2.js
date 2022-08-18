f91ae1ad7f2fee86425e099fd7fc914d5657a88b
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-08-18 16:21:06 -0400

    completes carousel feature

--- script2.js
+++ script2.js
@@ -1,65 +1,70 @@
-const URL2 = 'https://digimon-api.vercel.app/api/digimon/level/'
-
+const URL2 = "https://digimon-api.vercel.app/api/digimon/level/";
 
 // Cached element references
-const $inputLevel = $('#Level');
-const $formLevel = $('.level');
-const $owlCarousel = $('.owl-carousel');
+const $inputLevel = $("#Level");
+const $formLevel = $(".level");
+const $owlCarousel = $(".owl-carousel");
 
 // event listeners
-$formLevel.on('submit', handleSubmit);
-
-
+$formLevel.on("submit", handleSubmit);
 
 // functions
 handleSubmit();
 
-
 function handleSubmit(event) {
-    event && event.preventDefault();
-    
-    const level = $inputLevel.val() || 'In Training';
-    
-    if(!level) return;
-    
-    const promise = $.ajax(`${URL2}${level}`);
-    
-    promise.then(
-        function(data){;
-            renderLevel(data);
-            console.log(data)
-            $inputLevel.val('');
-        },
-        function(error){
-            alert('Error: Could not find Digimon in the database', error);
-            $input.val('');
-        }
-        )
+  event && event.preventDefault();
+
+  const level = $inputLevel.val() || "In Training";
+
+  if (!level) return;
+
+  const promise = $.ajax(`${URL2}${level}`);
+
+  promise.then(
+    function (data) {
+      renderLevel(data);
+      console.log(data);
+      $inputLevel.val("");
+    },
+    function (error) {
+      alert("Error: Could not find Digimon in the database", error);
+      $input.val("");
     }
-    
-    function renderLevel(digiData) {
-        for (let i = 0; i < digiData.length; i++) {
-            let digimon = digiData[i];
-            $owlCarousel.append(`
-            <div class="item">
+  );
+}
+
+function renderLevel(digiData) {
+  const cards = [];
+  const newCarousel = document.createElement("div");
+  $(newCarousel).addClass("new-carousel owl-carousel owl-theme");
+  $(".main").html(newCarousel);
+
+  for (let i = 0; i < digiData.length; i++) {
+    let digimon = digiData[i];
+    const div = document.createElement("div");
+
+    $(div).addClass("item").html(`
             <p><b>Name:</b> ${digimon.name}</p>
             <p><b>Level</b> : ${digimon.level}</p>
-            <img src="${digimon.img}" class="digi-img">
+            <div class="digi-img-wrapper">
+                <img src="${digimon.img}" class="digi-img">
             </div>
-            `);
-        }
-        
-        $owlCarousel.trigger('refresh.owl.carousel');
-        
-        $(document).ready(function(){
-            $owlCarousel.owlCarousel({
-                nav: true,
-                margin: 10,
-                autoplay: true,
-                autoplayTimeout: 2000,
-                autoplayHoverPause: true,
-            });
-        })
-    }
-    
-    
+        `);
+    cards.push(div);
+  }
+  $(newCarousel).html(cards);
+  const newOwl = $(".new-carousel");
+  newOwl.owlCarousel({
+    items: 4,
+    navigation: true,
+    loop: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+  });
+
+  $(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+  });
+}
