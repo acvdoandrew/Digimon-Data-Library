589c82c8c3016ab155f504de8dd7b2b1fc5194f5
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2022-08-18 14:17:50 -0400

    displays carousel in html

--- script2.js
+++ script2.js
@@ -2,49 +2,64 @@ const URL2 = 'https://digimon-api.vercel.app/api/digimon/level/'
 
 
 // Cached element references
-const $inputLevel = $('#Level')
-const $formLevel = $('.level')
+const $inputLevel = $('#Level');
+const $formLevel = $('.level');
+const $owlCarousel = $('.owl-carousel');
 
 // event listeners
-$formLevel.on('submit', handleSubmit)
+$formLevel.on('submit', handleSubmit);
 
 
 
 // functions
-
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        nav: true,
-        margin: 10,
-        autoplay: true,
-        autoplayTimeout: 2000,
-        autoplayHoverPause: true,
-    });
-})
-
 handleSubmit();
+
 
 function handleSubmit(event) {
     event && event.preventDefault();
     
-    const level = $inputLevel.val() || 'In training';
-
+    const level = $inputLevel.val() || 'In Training';
+    
     if(!level) return;
-
+    
     const promise = $.ajax(`${URL2}${level}`);
-
+    
     promise.then(
-        function(data){
-            console.log(data);
-            // render(data);
+        function(data){;
+            renderLevel(data);
+            console.log(data)
             $inputLevel.val('');
         },
         function(error){
             alert('Error: Could not find Digimon in the database', error);
             $input.val('');
         }
-    )
-};
-
-
-
+        )
+    }
+    
+    function renderLevel(digiData) {
+        for (let i = 0; i < digiData.length; i++) {
+            let digimon = digiData[i];
+            $owlCarousel.append(`
+            <div class="item">
+            <p><b>Name:</b> ${digimon.name}</p>
+            <p><b>Level</b> : ${digimon.level}</p>
+            <img src="${digimon.img}" class="digi-img">
+            </div>
+            `);
+        }
+        
+        $owlCarousel.trigger('refresh.owl.carousel');
+        
+        $(document).ready(function(){
+            $owlCarousel.owlCarousel({
+                nav: true,
+                margin: 10,
+                autoplay: true,
+                autoplayTimeout: 2000,
+                autoplayHoverPause: true,
+            });
+        })
+    }
+    
+    
